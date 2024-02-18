c88e5a3726bfeb770f16c42e229d00b9d5ee0b25
LoganCDane <l.c.dane@gmail.com>
2024-02-18 12:26:47 -0600

    Added a games tab -Logan
    Created a Games Tab that allows the user to schedule games based off of the 5 parameters that they enter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './Styles/App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import {Profile} from './Profile/Profile.js';
 import {Home} from './Home/Home.js';
+import { GamesList } from './Games/Games.js';
 //import {signInWithGoogle, signOutWithGoogle} from "./Firebase.js";
 import './Styles/index.css';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
             <Route exact path = "/" element = {<Home />}>/</Route>
             <Route exact path = "/Profile" element = {<Profile />}>/</Route>
+            <Route exact path = "/Games" element = {<GamesList />}>/</Route>
         </Routes>
       </Router>
     }
---
+++ src/Games/Games.js
@@ -0,0 +1,64 @@
+import { useNavigate } from "react-router-dom";
+import { getDocument } from "../Firebase.js";
+import { storeAndRetrieveData } from "../Firebase.js";
+import { storeFieldIntoDocument } from "../Firebase.js";
+export function GamesList() {
+    const navigate = useNavigate();
+    function goBack () {
+        //sends them back to the profile screen
+        navigate("/Profile");
+    }
+
+    function registerGame() {
+        const date = document.getElementById('date').value;
+        const time = document.getElementById('time').value;
+        const player1Email = document.getElementById('player1Email').value;
+        const player2Email = document.getElementById('player2Email').value;
+        const player3Email = document.getElementById('player3Email').value;
+
+        if(date && time && player1Email && player2Email && player3Email) {
+            const playerEmails = [player1Email, player2Email, player3Email];
+
+            console.log("Game registered!");
+        } else {
+            alert("Please fill all fields before registering.");
+        }
+    }
+
+    function storeGameIntoDB(date, time, playerEmails){
+        //storeFieldIntoDocument("Games", )
+    }
+
+    return (
+    <>
+    <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Game Registration</title>
+    </head>
+  
+
+    <div id="games"></div>
+
+    
+
+    <form id="gameForm">
+        <input type="date" id="date" placeholder="Date Inputted from Calendar" /><br />
+        <input type="time" id="time" placeholder="Desired Time" /><br />
+        <input type="email" id="player1Email" placeholder ="Player 1 Email" /><br />
+        <input type='email' id='player2Email' placeholder ='Player 2 Email' /><br />
+        <input type='email' id='player3Email' placeholder ='Player 3 Email' /><br /> 
+        <h1 className="py-5">
+            <button  className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={registerGame}>Register Game</button>
+        </h1> 
+        <h1 className="py-6">
+            <button  className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={goBack}>Go Back</button>
+        </h1> 
+    </form>
+    
+    </>
+    )
+
+
+
+}
